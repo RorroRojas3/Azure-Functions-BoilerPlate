e227d08a0efc5a6cbd9dbbfca2cf34af0bead47b
Rodrigo Rojas Garcia <rorro.irg@gmail.com>
2020-12-03 19:38:20 -0500

    Serilog implemented

---
+++ Rodrigo.Tech.BoilerPlate/Extensions/ServiceCollection/LoggingServiceCollection.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Rodrigo.Tech.Services.Helpers;
+using Serilog;
+using Serilog.Events;
+using System.IO;
+
+namespace Rodrigo.Tech.BoilerPlate.Extensions.ServiceCollection
+{
+    public static class LoggingServiceCollection
+    {
+        /// <summary>
+        ///     Adds Logging service
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        public static void AddLoggingServiceCollection(this IServiceCollection services, IConfiguration configuration)
+        {
+            var currentDirectory = DirectoryHelper.GetCurrentDirectory();
+            var logDirectory = Path.Combine(currentDirectory, "Logs");
+            if (!Directory.Exists(logDirectory))
+            {
+                Directory.CreateDirectory(logDirectory);
+            }
+
+            var logger = new LoggerConfiguration()
+                            .WriteTo.Console(LogEventLevel.Information)
+                            .ReadFrom.Configuration(configuration)
+                            .CreateLogger();
+            services.AddSingleton(logger);
+            services.AddLogging(l => l.AddSerilog(logger));
+
+            Log.Logger = new LoggerConfiguration()
+                        .WriteTo.Console(LogEventLevel.Information)
+                        .ReadFrom.Configuration(configuration)
+                        .CreateLogger();
+            services.AddSingleton(Log.Logger);
+        }
+    }
+}
--- Rodrigo.Tech.BoilerPlate/Startup.cs
+++ Rodrigo.Tech.BoilerPlate/Startup.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Rodrigo.Tech.BoilerPlate.Extensions.ServiceCollection;
 using Rodrigo.Tech.Model.AutoMapper;
+using Rodrigo.Tech.Services.Helpers;
+using System;
 
 [assembly: FunctionsStartup(typeof(Rodrigo.Tech.BoilerPlate.Startup))]
 namespace Rodrigo.Tech.BoilerPlate
@@ -10,6 +13,16 @@ namespace Rodrigo.Tech.BoilerPlate
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
+            var currentEnvironment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var jsonFile = currentEnvironment.Equals("local", StringComparison.OrdinalIgnoreCase) 
+                                    ? "appsettings.local.json" : "appsettings.json";
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(DirectoryHelper.GetCurrentDirectory())
+                .AddJsonFile(jsonFile, false, true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            builder.Services.AddLoggingServiceCollection(configuration);
             builder.Services.AddDatabaseServiceCollection();
             builder.Services.AddServicesServiceCollection();
             builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
